34106f068e8d756c3979f8430c0149ca674f3f17
Juyoung Kim <juyoungkim@Juyoungs-MacBook-Pro.local>
2021-05-06 17:10:25 -0400

    push

--- RSIBOT/static/chart.js
+++ RSIBOT/static/chart.js
@@ -37,7 +37,7 @@ var candleSeries = chart.addCandlestickSeries({
 });
 
 
-fetch('juyoungk-final-project.herokuapp.com/history')
+fetch('/history')
 	.then((r) => r.json())
 	.then(function (response) {
 
